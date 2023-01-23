6f95a532ea53bebfa4efefc37f45b23406298ee3
Akbar S <54757270+akbartus@users.noreply.github.com>
2023-01-22 21:54:02 -0500

    Update handwriting.js

--- js/handwriting.js
+++ js/handwriting.js
@@ -1,6 +1,3 @@
-/* Created by:ChenYuHo
-   Original code: https://github.com/ChenYuHo/handwriting.js
-*/
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
